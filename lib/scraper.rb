a18beaecf2524d348a0dd371ce60a577a14fbb42
ghemsley <ghemsley@protonmail.ch>
2020-11-27 16:51:42 -0600

    commit tree 66274510dcaa528b652b6975a56def2c0d4b896c

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open('http://flatironschool.com/')
+doc = Nokogiri::HTML(html)
+
+p doc.css('#id-ea783478-7bf2-5965-897f-a023bd94ac90 > div > div > div > div').text
